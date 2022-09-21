794b6d1ad6f5eb20e6de5e07da567387be3559af
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-21 10:48:15 -0300

    Agregamos tarea al arreglo de tareas y limpiamos el value del imput tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTarea from "./ListaTarea";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [arregloTareas, setArregloTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArregloTareas([...arregloTareas, tarea]);
+    setTarea('')
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex " controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
